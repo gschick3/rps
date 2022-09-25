072bfdfff2f7afb03f5490252b606e14a65d1d7a
Galen Schick <gschick3@gmail.com>
2022-09-25 15:26:46 -0500

    complete prototype game UI

--- game.js
+++ game.js
@@ -4,13 +4,24 @@ function getComputerChoice() {
 
 function compareHands(player, computer) {
     player = player.toLowerCase();
-    console.log(player, computer);
+    
     options = {
         'rock': 'scissors',
         'paper': 'rock',
         'scissors': 'paper'            
     }
-    if (player === computer) console.log("It's a tie");
-    else if (options[player] === computer) console.log("Player wins!");
-    else console.log("Player loses :(");
+
+    document.querySelector('.computer').textContent = `${player.toUpperCase()} vs ${computer.toUpperCase()}`;
+
+    const result = document.querySelector('.result');
+    if (player === computer) result.textContent = "It's a tie";
+    else if (options[player] === computer) result.textContent = "You win!";
+    else result.textContent = "You lose :(";
 }
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => compareHands(button.id, getComputerChoice()))
+});
